cbb858b7c2374a0a52a21c2c60e5c3c1c70c6363
Alex W <runthesun@gmail.com>
2018-12-17 16:02:49 -0800

    initial commit

---
+++ apps/barcode_scanner/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BarcodeScannerConfig(AppConfig):
+    name = 'barcode_scanner'
---
+++ apps/barcode_scanner/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.root),
+    url(r'upload$', views.upload_file),
+    url(r'read$', views.read_barcode),
+]
